cdb6daad7add8256231498a9a3d952c3cc31cbbb
NovakVed <vednovak@gmail.com>
2024-08-03 21:33:52 +0200

    refactor

--- arabicsToRoman/dumb/ArabicsToRomanDumb.java
+++ arabicsToRoman/dumb/ArabicsToRomanDumb.java
@@ -10,73 +10,73 @@ class ArabicsToRomanDumb {
     }
 
     public static String intToRoman(int num) {
-        String result = "";
+        StringBuilder result = new StringBuilder();
 
         while (num >= 1000) {
-            result += "M";
+            result.append("M");
             num -= 1000;
         }
 
         if (num >= 900) {
-            result += "CM";
+            result.append("CM");
             num -= 900;
         }
 
         if (num >= 500) {
-            result += "D";
+            result.append("D");
             num -= 500;
         }
 
         if (num >= 400) {
-            result += "CD";
+            result.append("CD");
             num -= 400;
         }
 
         while (num >= 100) {
-            result += "C";
+            result.append("C");
             num -= 100;
         }
 
         if (num >= 90) {
-            result += "XC";
+            result.append("XC");
             num -= 90;
         }
 
         if (num >= 50) {
-            result += "L";
+            result.append("L");
             num -= 50;
         }
 
         if (num >= 40) {
-            result += "XL";
+            result.append("XL");
             num -= 40;
         }
 
         while (num >= 10) {
-            result += "X";
+            result.append("X");
             num -= 10;
         }
 
         if (num == 9) {
-            result += "IX";
-            return result;
+            result.append("IX");
+            return result.toString();
         }
 
         if (num > 4) {
-            result += "V";
+            result.append("V");
             num -= 5;
         }
 
         if (num == 4) {
-            result += "IV";
-            return result;
+            result.append("IV");
+            return result.toString();
         }
 
         while (num > 0) {
-            result += "I";
+            result.append("I");
             num -= 1;
         }
 
-        return result;
+        return result.toString();
     }
 }
