9ae363ed0f0dacfdd14de48603648556eef8f881
Vedran Novak <9297079+NovakVed@users.noreply.github.com>
2024-08-07 23:11:51 +0200

    add TopKFrequentElement.java

--- topKFrequentElements/TopKFrequentElements.java
+++ topKFrequentElements/TopKFrequentElements.java
@@ -1,17 +1,17 @@
 package topKFrequentElements;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 /**
  * 347. TopKFrequentElements
  *
- * @link <a href="https://leetcode.com/problems/top-k-frequent-elements/description/">347. TopKFrequentElements</a>
+ * @link <a href=
+ *       "https://leetcode.com/problems/top-k-frequent-elements/description/">347.
+ *       TopKFrequentElements</a>
  */
 public class TopKFrequentElements {
 
@@ -25,18 +25,6 @@ public class TopKFrequentElements {
     public static int[] topKFrequent(int[] nums, int k) {
         Set<Integer> visited = new HashSet<>();
         Map<Integer, Integer> map = new HashMap<>();
-        for (var i : nums) {
-            if (visited.add(i)) {
-                map.put(i, map.getOrDefault(i, 0) + 1);
-            }
-        }
-
-        List<Integer> result = new ArrayList<>();
-        for (var i : visited) {
-            if (map.get(i) > k) {
-                result.add(i);
-            }
-        }
 
         return new int[] {};
     }
