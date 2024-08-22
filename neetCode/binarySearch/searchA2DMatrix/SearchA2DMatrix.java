8e65ad726986f3020fa25efa10a710f119a64dc1
Vedran Novak <9297079+NovakVed@users.noreply.github.com>
2024-08-22 21:51:36 +0200

    add doc to SearchA2DMatrix.java

--- neetCode/binarySearch/searchA2DMatrix/SearchA2DMatrix.java
+++ neetCode/binarySearch/searchA2DMatrix/SearchA2DMatrix.java
@@ -1,5 +1,10 @@
 package neetCode.binarySearch.searchA2DMatrix;
 
+/**
+ * 74. Search a 2D Matrix
+ * {@link} <a href="https://leetcode.com/problems/search-a-2d-matrix/">74.
+ * Search a 2D Matrix</a>
+ */
 class SearchA2DMatrix {
     public void main(String[] args) {
         int[][] matrix = new int[][] { { 1, 3, 5, 7 }, { 10, 11, 16, 20 }, { 23, 30, 34, 60 } };
