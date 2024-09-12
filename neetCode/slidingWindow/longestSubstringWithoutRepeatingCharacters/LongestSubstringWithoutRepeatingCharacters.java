1a86621f4c1d58126b17a40aa167dd14b5071e75
Vedran Novak <9297079+NovakVed@users.noreply.github.com>
2024-09-12 23:48:08 +0200

    add LongestSubstringWithoutRepeatingCharacters

--- neetCode/slidingWindow/longestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.java
+++ neetCode/slidingWindow/longestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,5 +1,8 @@
 package neetCode.slidingWindow.longestSubstringWithoutRepeatingCharacters;
 
+import java.util.HashSet;
+import java.util.Set;
+
 class LongestSubstringWithoutRepeatingCharacters {
     public static void main(String[] args) {
         String s = "zxyzxyz";
@@ -7,6 +10,18 @@ class LongestSubstringWithoutRepeatingCharacters {
     }
 
     public static int lengthOfLongestSubstring(String s) {
-        return 0;
+        int l = 0;
+        int ans = 0;
+
+        Set<Character> visited = new HashSet<>();
+        for (int r = 0; r < s.length(); r++) {
+            while (visited.contains(s.charAt(r))) {
+                visited.remove(s.charAt(l++));
+            }
+            visited.add(s.charAt(r));
+            ans = Math.max(ans, r - l + 1);
+        }
+
+        return ans;
     }
 }
