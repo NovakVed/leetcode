6eede32d30895a7ff7de88d2fa2fc5a1a2c457e4
NovakVed <vednovak@gmail.com>
2024-09-12 17:42:35 +0200

    add LongestSubstringWithoutRepeatingCharacters.java

---
+++ neetCode/slidingWindow/longestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,12 @@
+package neetCode.slidingWindow.longestSubstringWithoutRepeatingCharacters;
+
+class LongestSubstringWithoutRepeatingCharacters {
+    public static void main(String[] args) {
+        String s = "zxyzxyz";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        return 0;
+    }
+}
