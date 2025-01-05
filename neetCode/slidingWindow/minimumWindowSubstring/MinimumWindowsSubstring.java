56955c90747d645040fbd793a602b1871d62fef3
Vedran Novak <9297079+NovakVed@users.noreply.github.com>
2025-01-05 23:10:47 +0100

    update .gitignore

---
+++ neetCode/slidingWindow/minimumWindowSubstring/MinimumWindowsSubstring.java
@@ -0,0 +1,20 @@
+package neetCode.slidingWindow.minimumWindowSubstring;
+
+/**
+ * 76. Minimum Window Substring
+ * {@link} <a href=
+ * "https://leetcode.com/problems/minimum-window-substring/">76. Minimum Window
+ * Substring</a>
+ */
+class MinimumWindowsSubstring {
+    public static void main(String[] args) {
+        String s = "ADOBECODEBANC";
+        String t = "ABC";
+
+        System.out.println(minWindow(s, t));
+    }
+
+    public static String minWindow(String s, String t) {
+
+    }
+}
