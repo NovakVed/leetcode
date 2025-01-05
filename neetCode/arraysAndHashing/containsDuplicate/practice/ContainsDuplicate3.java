c001c10d60c310c0b444cd849ebb2ac26e7df436
Vedran Novak <9297079+NovakVed@users.noreply.github.com>
2025-01-05 18:03:46 +0100

    add ContainsDuplicate3.java

---
+++ neetCode/arraysAndHashing/containsDuplicate/practice/ContainsDuplicate3.java
@@ -0,0 +1,22 @@
+package practice;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate3 {
+    public static void main(String[] args) {
+        int[] arr = new int[] { 1, 2, 3, 1 };
+        System.out.println(containsDuplicate(arr)
+                ? "Contains duplicates"
+                : "Does not contain duplicates");
+    }
+
+    private static boolean containsDuplicate(int[] arr) {
+        Set<Integer> covered = new HashSet<>();
+        for (int i : arr)
+            if (!covered.add(i))
+                return true;
+
+        return false;
+    }
+}
