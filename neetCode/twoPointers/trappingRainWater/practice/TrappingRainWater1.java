2023fc88fafef7ffe79863a279e7110c091f73a5
Vedran Novak <9297079+NovakVed@users.noreply.github.com>
2025-01-11 01:15:10 +0100

    add TrappingRainWater1.java

--- neetCode/twoPointers/trappingRainWater/practice/TrappingRainWater1.java
+++ neetCode/twoPointers/trappingRainWater/practice/TrappingRainWater1.java
@@ -1,6 +1,6 @@
 package neetCode.twoPointers.trappingRainWater.practice;
 
-public class TrappingRainWater {
+public class TrappingRainWater1 {
     public static void main(String[] args) {
         int[] height = new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
         System.out.println(trap(height));
@@ -8,20 +8,19 @@ public class TrappingRainWater {
 
     private static int trap(int[] height) {
         int l = 0, r = height.length - 1;
-        int leftMax = height[l], rightMax = height[r];
+        int maxLeft = height[0], maxRight = height[r];
         int trappedWater = 0;
         while (l < r) {
-            if (leftMax < rightMax) {
+            if (maxLeft < maxRight) {
                 l++;
-                leftMax = Math.max(leftMax, height[l]);
-                trappedWater += leftMax - height[l];
+                maxLeft = Math.max(maxLeft, height[l]);
+                trappedWater += maxLeft - height[l];
             } else {
                 r--;
-                rightMax = Math.max(rightMax, height[r]);
-                trappedWater += rightMax - height[r];
+                maxRight = Math.max(maxRight, height[r]);
+                trappedWater += maxRight - height[r];
             }
         }
-
         return trappedWater;
     }
 }
