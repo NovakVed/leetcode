16426252140bc9976f02ea20df812aa7a6f7ef28
NovakVed <vednovak@gmail.com>
2025-01-13 23:34:41 +0100

    add BestTimeToBuyAndSellStock1

--- neetCode/slidingWindow/bestTimeToBuyAndSellStock/practice/BestTimeToBuyAndSellStock1.java
+++ neetCode/slidingWindow/bestTimeToBuyAndSellStock/practice/BestTimeToBuyAndSellStock1.java
@@ -1,23 +1,17 @@
 package neetCode.slidingWindow.bestTimeToBuyAndSellStock.practice;
 
-class BestTimeToBuyAndSellStock {
+class BestTimeToBuyAndSellStock1 {
     public static void main(String[] args) {
-        // int[] prices = new int[] { 7, 1, 5, 3, 6, 4 };
-        int[] prices = new int[] { 1, 2 };
+        int[] prices = new int[] { 7, 1, 5, 3, 6, 4 };
         System.out.println(maxProfit(prices));
     }
 
     public static int maxProfit(int[] prices) {
-        int l = 0;
-        int profit = 0;
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[l] > prices[i]) {
-                prices[l] = prices[i];
-            }
-
-            if (prices[l] < prices[i]) {
-                profit = Math.max(profit, prices[i] - prices[l]);
-            }
+        int l = 0, profit = 0;
+        for (int r = 1; r < prices.length; r++) {
+            if (prices[l] > prices[r])
+                l = r;
+            profit = Math.max(profit, prices[r] - prices[l]);
         }
         return profit;
     }
