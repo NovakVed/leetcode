411ea4babc4f72073884fd786cb59c8a34c641c8
NovakVed <vednovak@gmail.com>
2025-01-21 23:35:31 +0100

    refactor TopKFrequentElements2

--- neetCode/arraysAndHashing/topKFrequentElements/practice/TopKFrequentElements2.java
+++ neetCode/arraysAndHashing/topKFrequentElements/practice/TopKFrequentElements2.java
@@ -18,17 +18,14 @@ public class TopKFrequentElements2 {
         List<Integer>[] buckets = new List[nums.length + 1];
         Map<Integer, Integer> count = new HashMap<>();
 
-        for (int i = 0; i < buckets.length; i++) {
+        for (int i = 0; i < buckets.length; i++)
             buckets[i] = new ArrayList<>();
-        }
 
-        for (int num : nums) {
+        for (int num : nums)
             count.put(num, count.getOrDefault(num, 0) + 1);
-        }
 
-        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+        for (Map.Entry<Integer, Integer> entry : count.entrySet())
             buckets[entry.getValue()].add(entry.getKey());
-        }
 
         var result = new int[k];
         int index = 0;
