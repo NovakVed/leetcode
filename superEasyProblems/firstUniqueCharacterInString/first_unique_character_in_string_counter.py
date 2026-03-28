e49adfecee54afd70f35c62b8c23b2f58adcb3f5
Vedran Novak <vednovak@gmail.com>
2026-03-28 13:41:15 +0100

    add firstUniqueChar optimized

---
+++ superEasyProblems/firstUniqueCharacterInString/first_unique_character_in_string_counter.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        count = Counter(s)
+        for i, char in enumerate(s):
+            if count[char] == 1:
+                return i
+        return -1
+
+
+s = Solution()
+print(s.firstUniqChar('leetcode'))
