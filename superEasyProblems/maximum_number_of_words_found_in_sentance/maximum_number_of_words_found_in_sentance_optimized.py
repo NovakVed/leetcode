784468ac720e2a0035ee337bd220f32e5e72ceba
Vedran Novak <vednovak@gmail.com>
2026-03-31 23:57:39 +0200

    add maximum number of words found in a list

---
+++ superEasyProblems/maximum_number_of_words_found_in_sentance/maximum_number_of_words_found_in_sentance_optimized.py
@@ -0,0 +1,6 @@
+class Solution:
+    def mostWordsFound(self, sentences: List[str]) -> int:
+        return max(len(sentence.split()) for sentence in sentences)
+        
+s = Solution()
+print(s.mostWordsFound(["alice and bob love leetcode", "i think so too", "this is great thanks very much"]))
