a7bd8a774e991254949fa2157cb9e443cb002e1c
Vedran Novak <vednovak@gmail.com>
2026-03-31 23:58:21 +0200

    refactor

--- superEasyProblems/maximum_number_of_words_found_in_sentance/maximum_number_of_words_found_in_sentance.py
+++ superEasyProblems/maximum_number_of_words_found_in_sentance/maximum_number_of_words_found_in_sentance.py
@@ -5,6 +5,6 @@ class Solution:
             temp = len(sentence.split())
             max_words = max(max_words, temp)
         return max_words
-        
+
 s = Solution()
 print(s.mostWordsFound(["alice and bob love leetcode", "i think so too", "this is great thanks very much"]))
