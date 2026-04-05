9d9588023b8dcf3cfdf2d03ba88d03ed42b1e2fa
Vedran Novak <vednovak@gmail.com>
2026-04-05 23:59:01 +0200

    add comment

--- superEasyProblems/1486_xor_operation_in_an_array/xor_operation_in_an_array.py
+++ superEasyProblems/1486_xor_operation_in_an_array/xor_operation_in_an_array.py
@@ -1,3 +1,6 @@
+# 1486. XOR Operation in an Array
+# https://leetcode.com/problems/xor-operation-in-an-array/description/
+# https://leetcode.com/problems/xor-operation-in-an-array/solutions/7686132/o1-time-o1-space-python-solution-a-compr-jvji/?envType=problem-list-v2&envId=prshgx6i
 class Solution:
     def xorOperation(self, n: int, start: int) -> int:
         result = 0
