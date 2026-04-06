ee619bf54a5af8fa74c736c991ab9ef4855ddfe0
Vedran Novak <vednovak@gmail.com>
2026-04-06 23:16:20 +0200

    add toLowerCase built in solution

---
+++ superEasyProblems/709_to_lower_case/to_lower_case.py
@@ -0,0 +1,9 @@
+# 709 To Lower Case
+# https://leetcode.com/problems/to-lower-case/description/
+# Explanation: https://leetcode.com/problems/to-lower-case/?envType=problem-list-v2&envId=prshgx6i
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
+
+s = Solution()
+print(s.toLowerCase('VEKI'))
