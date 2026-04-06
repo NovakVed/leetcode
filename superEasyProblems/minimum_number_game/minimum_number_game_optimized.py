d2f4f1bf36900dd051ab08202cb41270a2993a65
Vedran Novak <vednovak@gmail.com>
2026-04-07 01:19:19 +0200

    add optimized minimum number game solution

--- superEasyProblems/minimum_number_game/minimum_number_game_optimized.py
+++ superEasyProblems/minimum_number_game/minimum_number_game_optimized.py
@@ -3,15 +3,13 @@
 import sys
 from typing import List
 
-
+# space complexity O(1) if not counting output
 class Solution:
     def numberGame(self, nums: List[int]) -> List[int]:
-        result = [] # space complexity O(n)
         nums.sort()
         for i in range(0, len(nums), 2):
-            result.append(nums[i + 1])
-            result.append(nums[i])
-        return result
+            nums[i], nums[i+1] = nums[i+1], nums[i]
+        return nums
 
 s = Solution()
 print(s.numberGame([5, 4, 2, 3]))
