3bb9a7c2b01d854db21c6a442fe82c0beb10a9bc
Vedran Novak <vednovak@gmail.com>
2026-04-08 23:20:18 +0200

    add description on numberOfChangingKeys

--- superEasyProblems/3019_number_of_changing_keys/number_of_changing_keys.py
+++ superEasyProblems/3019_number_of_changing_keys/number_of_changing_keys.py
@@ -1,10 +1,12 @@
+# 3019. Number of Changing Keys
+# https://leetcode.com/problems/number-of-changing-keys/description/?envType=problem-list-v2&envId=prshgx6i
 class Solution:
     def countKeyChanges(self, s: str) -> int:
-        count = 0
+        changes = 0
         for i in range(1, len(s)):
             if s[i - 1].lower() != s[i].lower():
-                count += 1
-        return count
+                changes += 1
+        return changes
 
 s = Solution()
 print(s.countKeyChanges('aAbBcC'))
