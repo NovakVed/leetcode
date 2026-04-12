3694a5260605eb67c87ffa2e2275dd182407c260
Vedran Novak <vednovak@gmail.com>
2026-04-12 21:29:59 +0200

    add description

--- superEasyProblems/2942_find_words_containing_character/find_words_containing_character.py
+++ superEasyProblems/2942_find_words_containing_character/find_words_containing_character.py
@@ -2,11 +2,12 @@
 # https://leetcode.com/problems/find-words-containing-character/description/?envType=problem-list-v2&envId=prshgx6i
 from typing import List
 
-
+# Time complexity: O(n * m)
+# Space complexity: O(1) The space required for the return variable is not included in the calculation.
 class Solution:
     def findWordsContaining(self, words: List[str], x: str) -> List[int]:
         res=[]
-        for i in range(len(words)):
-            if x in words[i]:
+        for i in range(len(words)): # this is O(n)
+            if x in words[i]: # this is also O(m)
                 res.append(i)
         return res
