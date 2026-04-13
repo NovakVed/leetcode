1e4c7fe32364fb6514ca4a6571956b8f7817c2b5
Vedran Novak <vednovak@gmail.com>
2026-04-13 09:19:20 +0200

    remove blank row

--- superEasyProblems/count_the_digits_that_divide_a_number/count_the_digits_that_divide_a_number.py
+++ superEasyProblems/count_the_digits_that_divide_a_number/count_the_digits_that_divide_a_number.py
@@ -1,6 +1,5 @@
 # 2520. Count the Digits That Divide a Number
 # https://leetcode.com/problems/count-the-digits-that-divide-a-number/description/?envType=problem-list-v2&envId=prshgx6i
-
 class Solution:
     def countDigits(self, num: int) -> int:
         count = 0
