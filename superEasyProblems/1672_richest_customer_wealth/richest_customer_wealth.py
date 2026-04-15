ac1443a370d0487819667d06b0b3fc354c215770
Vedran Novak <vednovak@gmail.com>
2026-04-15 22:53:31 +0200

    add maximumWealth optimized

---
+++ superEasyProblems/1672_richest_customer_wealth/richest_customer_wealth.py
@@ -0,0 +1,11 @@
+# 1672. Richest Customer Wealth
+# https://leetcode.com/problems/richest-customer-wealth/description/?envType=problem-list-v2&envId=prshgx6i
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        output = 0
+        for account in accounts:
+            output = max(output, sum(account))
+        return output
+
+s = Solution()
+print(s.maximumWealth([[1,2,3],[3,2,1]]))
