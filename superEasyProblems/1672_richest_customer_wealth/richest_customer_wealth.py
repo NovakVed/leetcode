87badb655a4b12dfb45c8d48c2e5d1b053ad0fce
Vedran Novak <vednovak@gmail.com>
2026-04-16 21:30:58 +0200

    add list import

--- superEasyProblems/1672_richest_customer_wealth/richest_customer_wealth.py
+++ superEasyProblems/1672_richest_customer_wealth/richest_customer_wealth.py
@@ -1,5 +1,8 @@
 # 1672. Richest Customer Wealth
 # https://leetcode.com/problems/richest-customer-wealth/description/?envType=problem-list-v2&envId=prshgx6i
+from typing import List
+
+
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
         output = 0
